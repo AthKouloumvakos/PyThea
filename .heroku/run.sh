631a4775bc14017da9e0860e21241a5e48fb212b
AKoulou <athkouloumvakos@gmail.com>
2022-09-15 19:20:46 -0400

    Removes any file related to heroku
    These files was to deploy the application to Heroku with is no more a free option. This commit removes everything related.

--- .heroku/run.sh
+++
@@ -1,4 +0,0 @@
-find $BUILD_DIR -type f -name "libvtkAcceleratorsVTKm-9.0.so" -exec rm -f {} \;
-find $BUILD_DIR -type f -name "libvtkm_filter-9.0.a" -exec rm -f {} \;
-find $BUILD_DIR -type f -name "libvtkm_cont-9.0.a" -exec rm -f {} \;
-find $BUILD_DIR -type f -name "all.py" -exec sed -i -e 's/from .vtkAcceleratorsVTKm/#from .vtkAcceleratorsVTKm/g' {} \;
--- setup.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p ~/.streamlit/
-echo "[general]
-email = \"@gmail.com\""  > ~/.streamlit/credentials.toml
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = true"  >> ~/.streamlit/config.toml
