cb04aff8a8350d37f12f98eb27d0dbc702513ffd
AKoulou <athkouloumvakos@gmail.com>
2024-01-13 10:11:49 -0500

    Improves and adds hek_client tests
    Improves the test_hek_client and Adds test_hek_client_flare_data
    - Tests that the HEK client returns a HEKTable
    - Tests that hek_query results for EventType('FL') have consistent format
    - Test that the returned flare start-max-end times are always the same.
    - Test that the returned flare class is always the same.

--- PyThea/test/test_remote_clients.py
+++ PyThea/test/test_remote_clients.py
@@ -2,24 +2,53 @@
 Test the remote clients
 """
 
+import numpy as np
 import pytest
-from sunpy.net import attrs, hek
+from astropy.time import Time
+from sunpy.net import attrs as a
+from sunpy.net import hek
+
+
+def time_query_hek():
+    for query in [a.Time('2011/08/09 07:23:56', '2011/08/09 12:40:29'),
+                  a.Time('2012/09/10 08:24:57', '2012/09/10 13:41:30'),
+                  a.Time('2013/10/11 09:25:58', '2013/10/11 14:42:31'), ]:
+        yield query
 
 
 @pytest.mark.remote_data
-def test_hek_client():
+@pytest.mark.parametrize('time_query', time_query_hek())
+def test_hek_client(time_query):
     """
-    Tests that the HEK client returns a HEKTable
+    Tests that the HEK client returns a HEKTable and the hek_query results for EventType('FL') have consistent format
     """
+    h = hek.HEKClient()
+    hek_query = h.search(time_query, a.hek.EventType('FL'))
+    assert type(hek_query) == hek.hek.HEKTable
+
+    assert isinstance(hek_query['event_starttime'], Time)
+    assert isinstance(hek_query['event_peaktime'], Time)
+    assert isinstance(hek_query['event_endtime'], Time)
+    assert all(isinstance(h, np.str_) for h in hek_query['fl_goescls'])
+
+
+@pytest.mark.remote_data
+def test_hek_client_flare_data():
+    """
+    Tests that the HEK client returns always the same results (flare class, ...)
+    """
+
     startTime = '2011/08/09 07:23:56'
     endTime = '2011/08/09 12:40:29'
     eventType = 'FL'
 
-    hekTime = attrs.Time(startTime, endTime)
-    hekEvent = attrs.hek.EventType(eventType)
+    hekTime = a.Time(startTime, endTime)
+    hekEvent = a.hek.EventType(eventType)
 
     h = hek.HEKClient()
-    hek_query = h.search(hekTime, hekEvent)
-    assert type(hek_query) == hek.hek.HEKTable
 
-    # Test that the hek_query['event_peaktime'] is <class 'astropy.time.core.Time'>
+    hek_query = h.search(hekTime, hekEvent, a.hek.FL.GOESCls > 'B1.0', a.hek.OBS.Observatory == 'GOES')
+    assert all(hek_query['fl_goescls'] == ['C1.4', 'X6.9'])
+    assert all(hek_query['event_starttime'] == Time(['2011-08-09 07:19:00.000', '2011-08-09 07:48:00.000']))
+    assert all(hek_query['event_peaktime'] == Time(['2011-08-09 07:23:00.000', '2011-08-09 08:05:00.000']))
+    assert all(hek_query['event_endtime'] == Time(['2011-08-09 07:27:00.000', '2011-08-09 08:08:00.000']))
