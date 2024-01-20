3a49f496146b2a3f51d8557f4aba6e5ee87dd30b
AKoulou <athkouloumvakos@gmail.com>
2024-01-19 21:43:06 -0500

    Adds the test_vso_search
    - Tests that Fido.search returns always the same number of files from VSO.

--- PyThea/test/test_remote_clients.py
+++ PyThea/test/test_remote_clients.py
@@ -5,8 +5,11 @@ Test the remote clients
 import numpy as np
 import pytest
 from astropy.time import Time
+from sunpy.net import Fido
 from sunpy.net import attrs as a
 from sunpy.net import hek
+
+from PyThea.config.selected_imagers import imager_dict
 
 
 def time_query_hek():
@@ -52,3 +55,34 @@ def test_hek_client_flare_data():
     assert all(hek_query['event_starttime'] == Time(['2011-08-09 07:19:00.000', '2011-08-09 07:48:00.000']))
     assert all(hek_query['event_peaktime'] == Time(['2011-08-09 07:23:00.000', '2011-08-09 08:05:00.000']))
     assert all(hek_query['event_endtime'] == Time(['2011-08-09 07:27:00.000', '2011-08-09 08:08:00.000']))
+
+
+def imager_query():
+    for imager in imager_dict.keys():
+        yield imager
+
+
+@pytest.mark.remote_data
+@pytest.mark.parametrize('imager', imager_query())
+def test_vso_search(imager):
+    """
+    Tests that the Fido.search returns always the same number of files from VSO.
+    """
+    file_num = {'AIA': [60, 61],
+                'LC2': [5], 'LC3': [5],
+                'COR2A': [7], 'COR2B': [7],
+                'EUVIA': [11], 'EUVIB': [11],
+                'COR1A': [18, 34], 'COR1B': [17, 66],
+                'HI1A': [1, 2, 3], 'HI1B': [1, 2],
+                'HI2A': [1, 2], 'HI2B': [1], }
+
+    for query in [a.Time('2011/01/01 00:00:00', '2011/01/01 01:00:00'),
+                  a.Time('2012/01/01 00:00:00', '2012/01/01 01:00:00'),
+                  a.Time('2013/01/01 00:00:00', '2013/01/01 01:00:00'), ]:
+        args = imager_dict[imager][0]
+        result = Fido.search(query, *args)
+
+        assert len(result) == 1
+        assert 'vso' in result.keys()
+
+        assert result.file_num in file_num[imager]
