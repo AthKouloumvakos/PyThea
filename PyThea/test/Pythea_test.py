78fb4f6a4370005d345f9748ce0558bc7053ee82
AKoulou <athkouloumvakos@gmail.com>
2024-04-04 21:27:18 -0400

    Adds a test to verify the existence of Pythea's database directory

--- PyThea/test/Pythea_test.py
+++ PyThea/test/Pythea_test.py
@@ -6,3 +6,14 @@ Note
 Before you start the test the PyThea pkg should be in the python path
 export PYTHONPATH="${PYTHONPATH}:{top_level_dir_that_pythea_lives}/PyThea"
 """
+
+import os
+from pathlib import Path
+
+
+def test_database_dir_exists():
+    """
+    Tests that Pythea's database directory exists.
+    """
+    database_dir = os.path.join(Path.home(), 'PyThea')
+    assert os.path.exists(database_dir), f"PyThea's database directory '{database_dir}' does not exist."
