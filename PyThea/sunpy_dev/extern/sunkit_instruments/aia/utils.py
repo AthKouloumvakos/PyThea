01e631150fa15c426bbb282e4321a2a20968e283
AKoulou <athkouloumvakos@gmail.com>
2024-12-19 14:23:55 -0500

    Fixes a temporary bug with JSOC and AIA prep
    - JSOC is not working so AIA pointing correction cannot be applied. This is temporary solved here but the hash data have to be updated when JSOC becomes availiable again.

--- PyThea/sunpy_dev/extern/sunkit_instruments/aia/utils.py
+++ PyThea/sunpy_dev/extern/sunkit_instruments/aia/utils.py
@@ -1,3 +1,4 @@
+import warnings
 from astropy.time import TimeDelta
 from aiapy.calibrate import fix_observer_location, update_pointing
 
@@ -5,7 +6,11 @@ __all__ = ['prep_aia']
 
 
 def prep_aia(map_sequence):
-    map_sequence = [update_pointing(tmap) for tmap in map_sequence]
+    try:
+        map_sequence = [update_pointing(tmap) for tmap in map_sequence]
+    except:
+        warnings.warn('Prepare AIA maps failed, script proceeded without update_pointing. Check connection with JSOC.', UserWarning)
+
     map_sequence = [fix_observer_location(tmap) for tmap in map_sequence]
 
     for map_ in map_sequence:
